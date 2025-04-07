9fb4a6d5f723a0520d88b17403fb65f646d212a5
Juan Diego <juandiqa@hotmail.com>
2025-04-07 00:33:28 -0500

    Fin taller TypeScript2

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,32 @@ import { series } from './data.js';
 import { Serie } from './Serie.js';
 
 const seriesTbody: HTMLElement = document.getElementById("series")!;
+const detailContainer: HTMLElement = document.getElementById("detail")!;
 
 function renderSeriesInTable(series: Serie[]): void {
   series.forEach(c => {
-    let trElement = document.createElement("tr");
-    trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.name}</td>
-                           <td>${c.chanel}</td>
-                           <td>${c.seasons}</td>`;
+  let trElement = document.createElement("tr");
+  trElement.innerHTML = `<td>${c.id}</td>
+                          <td>${c.name}</td>
+                          <td>${c.chanel}</td>
+                          <td>${c.seasons}</td>`;
+  trElement.addEventListener("click", () => renderSerieDetail(c));
   seriesTbody.appendChild(trElement);
   });
 }
 
-function getTotalSeasons(series: Serie[]): number {
+function renderSerieDetail(serie: Serie): void {
+  detailContainer.innerHTML = `<div class="card" style="width: 18rem;">
+                               <img class="card-img-top" src="${serie.image}" alt="${serie.name}">
+                               <div class="card-body">
+                               <h5 class="card-title">${serie.name}</h5>
+                               <p class="card-text">${serie.description}</p>
+                               <a href="${serie.url}" class="btn btn-primary" target="_blank">Go to ${serie.chanel}</a>
+                                </div>
+                                </div>`;
+}
+
+function getAverageSeasons(series: Serie[]): number {
   let totalSeason: number = 0;
   let totalSeries: number = 0;
   series.forEach((serie) => totalSeries = totalSeries + 1);
@@ -24,17 +37,17 @@ function getTotalSeasons(series: Serie[]): number {
   }
   let average: number = 0;
   average = totalSeason / totalSeries;
-  return totalSeason;
+  return average;
 }
 
-function renderTotalSeasons(series: Serie[]): void {
-  const totalSeasons = getTotalSeasons(series);
-  const totalSeasonsElement = document.createElement("p");
-  totalSeasonsElement.textContent = `Seasons average: ${totalSeasons}`;
+function renderAverageSeasons(series: Serie[]): void {
+  const averageSeasons = getAverageSeasons(series);
+  const averageSeasonsElement = document.createElement("p");
+  averageSeasonsElement.textContent = `Seasons average: ${averageSeasons}`;
   
   const averageContainer = document.getElementById("seasons-average")!;
-  averageContainer.appendChild(totalSeasonsElement); 
+  averageContainer.appendChild(averageSeasonsElement); 
 }
 
 renderSeriesInTable(series);
-renderTotalSeasons(series);
+renderAverageSeasons(series);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,28 @@
 import { series } from './data.js';
 const seriesTbody = document.getElementById("series");
+const detailContainer = document.getElementById("detail");
 function renderSeriesInTable(series) {
     series.forEach(c => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.name}</td>
-                           <td>${c.chanel}</td>
-                           <td>${c.seasons}</td>`;
+                          <td>${c.name}</td>
+                          <td>${c.chanel}</td>
+                          <td>${c.seasons}</td>`;
+        trElement.addEventListener("click", () => renderSerieDetail(c));
         seriesTbody.appendChild(trElement);
     });
 }
-function getTotalSeasons(series) {
+function renderSerieDetail(serie) {
+    detailContainer.innerHTML = `<div class="card" style="width: 18rem;">
+                               <img class="card-img-top" src="${serie.image}" alt="${serie.name}">
+                               <div class="card-body">
+                               <h5 class="card-title">${serie.name}</h5>
+                               <p class="card-text">${serie.description}</p>
+                               <a href="${serie.url}" class="btn btn-primary" target="_blank">Go to ${serie.chanel}</a>
+                                </div>
+                                </div>`;
+}
+function getAverageSeasons(series) {
     let totalSeason = 0;
     let totalSeries = 0;
     series.forEach((serie) => totalSeries = totalSeries + 1);
@@ -20,14 +32,14 @@ function getTotalSeasons(series) {
     }
     let average = 0;
     average = totalSeason / totalSeries;
-    return totalSeason;
+    return average;
 }
-function renderTotalSeasons(series) {
-    const totalSeasons = getTotalSeasons(series);
-    const totalSeasonsElement = document.createElement("p");
-    totalSeasonsElement.textContent = `Seasons average: ${totalSeasons}`;
+function renderAverageSeasons(series) {
+    const averageSeasons = getAverageSeasons(series);
+    const averageSeasonsElement = document.createElement("p");
+    averageSeasonsElement.textContent = `Seasons average: ${averageSeasons}`;
     const averageContainer = document.getElementById("seasons-average");
-    averageContainer.appendChild(totalSeasonsElement);
+    averageContainer.appendChild(averageSeasonsElement);
 }
 renderSeriesInTable(series);
-renderTotalSeasons(series);
+renderAverageSeasons(series);
